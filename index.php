d59fe2e4dff99a034e6c215585a4b0ef543894c0
ArmNonthawat <99850674+ArmNonthawat@users.noreply.github.com>
2022-10-03 15:02:03 +0700

    เพิ่มไฟล์2ไฟล์
    เพิ่มไฟล์2ไฟล์

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<?php include("includes/header.php"); ?>
+</head>
+
+<body>
+<?php include("includes/navbar.php"); ?>
+<div class="container">
+    <!-- Start -->
+<h2>หน้าหลัก</h2>
+<hr>
+welcome นลธวัช สมศักดิ์ xxxx
+
+    <!-- End -->
+</div>
+
+</body>
+
+</html>
+<?php include("includes/footer.php"); ?>
+
